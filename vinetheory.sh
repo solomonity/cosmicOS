2808212b2e7cec6d37a5fe69c6bdbaf3b1fd994f
solomonity <adam@solomon-books.com>
2023-04-13 14:47:44 +0100

    vine theory

---
+++ vinetheory.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# This is the alternative type of sol streamer 
+# written by solomonity author of Cosmic Vine Theory
+# and probabilistic interface for quantum streaming
+
+while true; 
+do
+realvariable=$(shuf -n 1 tardis-number-data)
+ref=realvariable
+
+realvariable2=$(shuf -n 1 tardis-number-data)
+ref2=realvariable2
+
+realvariable3=$(shuf -n 1 tardis-number-data)
+ref3=realvariable3
+
+realvariable4=$(shuf -n 1 tardis-number-data)
+ref4=realvariable4
+
+realvariable5=$(shuf -n 1 tardis-number-data)
+ref5=realvariable5
+
+realvariable6=$(shuf -n 1 tardis-number-data)
+ref6=realvariable6
+
+realvariable7=$(shuf -n 1 tardis-number-data)
+ref7=realvariable7
+
+realvariable8=$(shuf -n 1 tardis-number-data)
+ref8=realvariable8
+
+realvariable9=$(shuf -n 1 tardis-number-data)
+ref9=realvariable9
+
+realvariable10=$(shuf -n 1 tardis-number-data)
+ref10=realvariable10
+
+realvariable11=$(shuf -n 1 tardis-number-data)
+ref11=realvariable11
+
+realvariable12=$(shuf -n 1 tardis-number-data)
+ref12=realvariable12
+
+realvariable13=$(shuf -n 1 tardis-number-data)
+ref13=realvariable13
+
+realvariable14=$(shuf -n 1 tardis-number-data)
+ref14=realvariable14
+
+realvariable15=$(shuf -n 1 tardis-number-data)
+ref15=realvariable15
+
+realvariable16=$(shuf -n 1 tardis-number-data)
+ref16=realvariable16
+
+
+
+
+realvariable17=$(shuf -n 1 tardis-number-data)
+ref17=realvariable17
+
+realvariable18=$(shuf -n 1 tardis-number-data)
+ref18=realvariable18
+
+realvariable19=$(shuf -n 1 tardis-number-data)
+ref19=realvariable19
+
+realvariable20=$(shuf -n 1 tardis-number-data)
+ref20=realvariable20
+
+realvariable21=$(shuf -n 1 tardis-number-data)
+ref21=realvariable21
+
+realvariable22=$(shuf -n 1 tardis-number-data)
+ref22=realvariable22
+
+realvariable23=$(shuf -n 1 tardis-number-data)
+ref23=realvariable23
+
+realvariable24=$(shuf -n 1 tardis-number-data)
+ref24=realvariable24
+
+realvariable25=$(shuf -n 1 tardis-number-data)
+ref25=realvariable25
+
+realvariable26=$(shuf -n 1 tardis-number-data)
+ref26=realvariable26
+
+realvariable27=$(shuf -n 1 tardis-number-data)
+ref27=realvariable27
+
+realvariable28=$(shuf -n 1 tardis-number-data)
+ref28=realvariable28
+
+
+
+
+echo "${!ref} ${!ref2} ${!ref3} ${!ref4} ${!ref5} ${!ref6} ${!ref7} ${!ref8} ${!ref9} ${!ref10} ${!ref11} ${!ref12} ${!ref13} ${!ref14} ${!ref15} ${!ref16} ${!ref17} ${!ref17} ${!ref18} ${!ref19} ${!ref20} ${!ref21} ${!ref22} ${!ref23} ${!ref24} "   # prints the contents of the real variable
+
+done
