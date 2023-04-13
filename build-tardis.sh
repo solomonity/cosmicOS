8a87edce09fd276d29547541f4a02574de72d8d1
solomonity <adam@solomon-books.com>
2023-04-13 14:51:34 +0100

    tardis sequencer component

---
+++ build-tardis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# BEGIN UNIVERSAL CONSTRUCTOR PROGRAM
+# Freeman Rights DEC 21 2012-
+# This is a builder assembler unit for the Ravencoin blockchain
+# SRC: Streaming Templeate
+# Design and concept by Adam Bull
+
+
+i=0;
+while true;
+do
+suffix=$(shuf -n 1 tardis-data)
+echo $suffix;
+((i++))
+
+
+
+done;
+
